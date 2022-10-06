71003433a22b5967d0c835facda5da9a84cefd2b
baotramngo97 <112080207+baotramngo97@users.noreply.github.com>
2022-10-06 12:45:43 +0700

    Add files via upload

---
+++ js/custom.js
@@ -0,0 +1,31 @@
+// JavaScript Document
+
+              new WOW().init();
+       $(document).ready(function(){
+  $(".nav-toggle").click(function(){
+    $(".menu").slideToggle("slow");
+  });
+});
+
+var typed = new Typed('.element', {
+               	strings: ["Nguyễn Công Thành"],
+       	 	typeSpeed:50,
+        		backSpeed:50,
+        		loop:true,
+        		});
+
+
+$('#banner').owlCarousel({
+    loop:true,
+    margin:0,
+    nav:true,
+    items:1,
+ 
+})
+lightbox.option({
+	'resizeDuration': 200,
+	'wrapAround': true,
+	'disableScrolling': true,
+	
+})
+
---
+++ js/moi.js
@@ -0,0 +1,7 @@
+// JavaScript Document
+var typed = new Typed('.element', {
+               	strings: ["Bảo Trâm"],
+       	 	typeSpeed:50,
+        		backSpeed:50,
+        		loop:true,
+        		});
